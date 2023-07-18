1c854b156ca259926a8b87151c01af39465b4f80
rawidaniel <rawidaniel96@gmail.com>
2023-07-18 09:33:15 +0300

    final

---
+++ nest-scratch/src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from "@nestjs/common"
+
+
+@Controller('/app')
+export class AppController{
+
+  @Get('/hi')
+  getRootRoute(){
+    return "Hi There!"
+  }
+
+  @Get('/bye')
+  getByeThere(){
+    return "Bye There!"
+  }
+}
